c4e2fc3e1cb90496c6b7c2cbb1f64b0983cd329a
AnkitJaiswal26 <64367253+AnkitJaiswal26@users.noreply.github.com>
2020-06-08 21:55:27 +0530

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,1 +1,13 @@
+function() {
+  var e = $(this).data("navigation-target"),
+    o = $(".sidebar-group .sidebar#" + e);
+  o.closest(".sidebar-group").find(".sidebar").removeClass("active"), o.addClass("active"), o.find("form input:first").focus(), $("[data-navigation-target]").removeClass("active"), $('[data-navigation-target="' + e + '"]').addClass("active"), $(".sidebar-group").removeClass("mobile-open"), o.closest(".sidebar-group").addClass("mobile-open"), t.Started.scrolllRun(!0)
+}
 
+$(document).ready(function(){
+	$('.profile').click(function(){
+		var e = $(this).data("navigation-target"),
+    	o = $(".sidebar-group .sidebar#" + e);
+  		o.closest(".sidebar-group").find(".sidebar").removeClass("active"), o.addClass("active"), o.find("form input:first").focus(), $("[data-navigation-target]").removeClass("active"), $('[data-navigation-target="' + e + '"]').addClass("active"), $(".sidebar-group").removeClass("mobile-open"), o.closest(".sidebar-group").addClass("mobile-open"), t.Started.scrolllRun(!0)
+	})
+})
